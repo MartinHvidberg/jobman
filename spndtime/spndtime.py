5cb0ee18150d997ef026f570c8982c5c4ef9fc0f
MartinHvidberg <Martin@Hvidberg.net>
2017-02-07 17:55:22 +0100

    intro spndtime

---
+++ spndtime/spndtime.py
@@ -0,0 +1,11 @@
+
+
+"""" Part of JobMan
+This program is intended for test of JobMan.
+It's only purpose is to be started in 100 different copies,
+to calculate some values, which is returned as results.
+More specific it's called with a commandline option, a number n
+between 1-100. It will then walk through all numbers n, n+100, n+200
+until reaching 10.000, and for each checking if that number is prime.
+Nothing more - quite pointless - but suited to test JobMan..."""
+
