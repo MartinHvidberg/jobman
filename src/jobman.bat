c958beae54c60f13fdf825ba9b5e2cd118d24f07
MartinHvidberg <Martin@Hvidberg.net>
2017-08-03 11:34:40 +0200

    spacy

--- src/jobman.bat
+++ src/jobman.bat
@@ -1,7 +1,7 @@
 :: Refresh files from template
 xcopy F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_client_template\jobman.bat /V /Y
 xcopy F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_client_template\jobman.py /V /Y
-xcopy F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_client_template\Executables /S /V /Y
+xcopy F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_client_template\Executables\*.* .\Executables /S /V /Y
 
 :: Run
 C:\Python27\python.exe jobman.py
