aa64b9aed26ad64eb5f2e61f0cf84be460c5934b
MartinHvidberg <martin@hvidberg.net>
2017-08-03 10:24:38 +0200

    new dir name

--- jobfabrik/udsigt/make_udsigt_runs.py
+++ jobfabrik/udsigt/make_udsigt_runs.py
@@ -212,8 +212,8 @@ if __name__=="__main__":
 
     num_shot_length = 2000 # SeptiView shoots 2km
     str_fn_cell_list_1km = "cell_samp_1km.txt"
-    str_main_workdir = r"F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_master_udsi\Available"  # Where the job-files go
-    str_safety = r"F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_master_udsi\Results_copy" # A hardcoded place where important results are copied for safe keeping
+    str_main_workdir = r"F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_master\Available"  # Where the job-files go
+    str_safety = r"F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_master\Results_copy" # A hardcoded place where important results are copied for safe keeping
 
     # open log file
     fil_log = open("make_udsigt_run.log", "w")
