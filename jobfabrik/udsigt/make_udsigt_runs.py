8baa3685e44b6e23063cda7c81b4c9254915d995
MartinHvidberg <martin@hvidberg.net>
2017-08-07 15:28:38 +0200

    MakeUdsigt.py - fix error

--- jobfabrik/udsigt/make_udsigt_runs.py
+++ jobfabrik/udsigt/make_udsigt_runs.py
@@ -221,7 +221,7 @@ def build_all_jobs(lst_all_cells_local, str_main_workdir_local):
             # Back out and clean up...
             fil_batch.write("\n:: Back out and clean up...\n")
             fil_batch.write("CD .." + "\n")
-            fil_batch.write("DEL /F /S /  {}\n".format(str_injob_work_dir))
+            fil_batch.write("DEL /F /S /Q  {}\n".format(str_injob_work_dir))
             fil_batch.write("RMDIR /S /Q {}\n".format(str_injob_work_dir))
 
             fil_batch.flush()
@@ -233,7 +233,7 @@ if __name__=="__main__":
 
     num_shot_length = 2000 # SeptiView shoots 2km
     str_fn_cell_list_1km = "cell_list_1km.txt" # "cell_samp_1km.txt", "cell_list_1km.txt"
-    str_main_workdir = r"F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_master\Available"  # Where the job-files go
+    str_main_workdir = r"F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_master\Pause"  # Where the job-files go
     str_safety = r"F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_master\Results_copy" # A hardcoded place where important results are copied for safe keeping
 
     # open log file
