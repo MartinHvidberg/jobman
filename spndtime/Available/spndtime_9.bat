f3c341ff87b9c915ae8d711dd050dd958f2acc04
MartinHvidberg <Martin@Hvidberg.net>
2017-08-24 09:12:42 +0200

    diverse småting

---
+++ spndtime/Available/spndtime_9.bat
@@ -0,0 +1,1 @@
+C:\Python27\python.exe ..\..\Executables\spndtime.py 9
---
+++ spndtime/copy_to_work.bat
@@ -0,0 +1,2 @@
+copy /V spndtime.py F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_master\Executables
+copy /V Available\*.* F:\PGV\Projektarbejdsmapper\P4\Software\JobMan\jobman_udsi_master\Available
